a6d7635da5c9a719b83919ec96418e2d4700919e
pfox72 <pfox72@iinet.net.au>
2022-03-17 14:59:58 +0800

    - added pspdfkit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,39 @@
 import React, { useState } from 'react';
-import { SafeAreaView, StatusBar, View } from "react-native";
+import { Platform, SafeAreaView, View } from "react-native";
 import { Footer } from './Footer';
 import { Header } from './Header';
+import PSPDFKitView from 'react-native-pspdfkit';
 
 const App = () => {
   const [currentPageNumber, setCurrentPageNumber] = useState(0);
 
+  const [documentFolder, setDocumentFolder] = useState(Platform.OS === 'android' ? 'file:///android_asset/' : '');
+
+  const onStateChange = (e: any) => {
+    if (e.currentPageIndex) {
+      setCurrentPageNumber(e.currentPageIndex);
+    }
+  };
+
   return (
     <SafeAreaView style={{ backgroundColor: "black", flexDirection: "column", flex: 1 }}>
-      <StatusBar backgroundColor={"black"} barStyle={"light-content"} />
-
       <Header />
 
       <View style={{ flex: 1, backgroundColor: "white" }}>
+        <PSPDFKitView
+          document={`${documentFolder}sample.pdf`}
+          configuration={{
+            showThumbnailBar: 'scrollable',
+            pageTransition: 'scrollContinuous',
+            scrollDirection: 'vertical',
+          }}
+          fragmentTag="PDF1"
+          style={{flex: 1}}
+          onStateChanged={onStateChange}
+        />
       </View>
       
-      <Footer text={`Page: ${currentPageNumber}`} />
+      <Footer text={`Page: ${currentPageNumber + 1}`} />
     </SafeAreaView>
   );
 };
